0f9d469d709d6678e78b80b1919f22fac0f118aa
Kaushik05122002 <103945974+Kaushik05122002@users.noreply.github.com>
2023-03-16 14:34:27 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,16 @@
+function add(x,y){
+    return x+y;
+}
+function sub(x,y){
+    return x-y;
+}
+function product(x,y){
+    return x*y;
+}
+function division(x,y){
+    return x/y;
+}
+exports.addition=add;
+exports.subtaction=sub;
+exports.product=product;
+exports.division=division;
